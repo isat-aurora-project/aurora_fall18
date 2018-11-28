03861debea2c605cbbf51506f1d5672e78300e8b
Phillip Stratos <phillipstratos@gmail.com>
2018-11-28 11:27:09 -0500

    UX Rough Draft

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <ion-app>
     <ion-header>
-      <ion-nav-bar class="bar-light">
+      <ion-navbar class="bar-light">
         <ion-nav-back-button></ion-nav-back-button>
-        <router-link  v-if="authenticated" to="/home" replace>Home |</router-link>
+        <router-link  v-if="authenticated" to="/home" replace>| Home |</router-link>
         <router-link v-if="authenticated" to="/upload"> Upload |</router-link>
-        <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace> Logout</router-link>
-      </ion-nav-bar>
+        <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace> Logout |</router-link>
+      </ion-navbar>
     </ion-header>
     <router-view @authenticated="setAuthenticated"/>
   </ion-app>
@@ -14,31 +14,31 @@
 </template>
 
 <script>
-  export default {
-    name:'App',
-    data(){
-      return{
-        authenticated: false,
-        mockAccount: {
-          username:"tucker",
-          password:"admin"
-        }
+export default {
+  name: "App",
+  data() {
+    return {
+      authenticated: false,
+      mockAccount: {
+        username: "tucker",
+        password: "admin"
       }
+    };
+  },
+  mounted() {
+    if (!this.authenticated) {
+      this.$router.replace({ name: "login" });
+    }
+  },
+  methods: {
+    setAuthenticated(status) {
+      this.authenticated = status;
     },
-    mounted(){
-      if(!this.authenticated){
-        this.$router.replace({ name: "login"});
-      }
-    },
-    methods: {
-      setAuthenticated(status){
-        this.authenticated=status;
-      },
-      logout(){
-        this.authenticated=false;
-      }
+    logout() {
+      this.authenticated = false;
     }
   }
+};
 </script>
 
 <style lang="scss">
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,71 +2,72 @@
     <ion-content padding="true" class="manual-ios-statusbar-padding">
         <div class="spacer" style="width: 300px; height: 60px;"></div>
         <div>
-        <img src="img/UpMXuuD3RRmVOyUcWSi2_aurora.png" style="display: block; width: 100%; height: auto; margin-left: auto; margin-right: auto;">
+        <img src="img/UpMXuuD3RRmVOyUcWSi2_aurora.png" style="display: block; width: auto; height: auto; margin-left: auto; margin-right: auto;">
         </div>
         <form id="login-form1" class="list">
         <ion-list id="login-list1">
             <ion-item>
-            <ion-label class="item item-input" id="login-input1">
-            <span class="input-label">Username</span>
+            <ion-label class="item item-input" id="login-input1" style="display: block; width: 300px; height: 49px; margin-left: auto; margin-right: auto;">
+            <span class="input-label">Username </span>
             <input type="text" name="username" v-model="input.username" placeholder="Username" />
             </ion-label>
             </ion-item>
             <ion-item>
-            <ion-label class="item item-input" id="login-input2">
-            <span class="input-label">Password</span>
+            <ion-label class="item item-input" id="login-input2" style="display: block; width: 300px; height: 49px; margin-left: auto; margin-right: auto;">
+            <span class="input-label">Password </span>
             <input type="password" name="password" v-model="input.password" placeholder="Password" />
             </ion-label>
             </ion-item>
         </ion-list>
         <div class="spacer" style="width: 300px; height: 40px;"></div>
-        <ion-button type="button" v-on:click="login()" id="login-button1" class="button button-stable button-block">Login</ion-button>
+        <ion-button type="button" v-on:click="login()" id="login-button1" class="button button-stable button-block" style="display: block; width: 310px; height: 57px; margin-left: auto; margin-right: auto;">Login</ion-button>
         
         <div class="spacer" style="height: 40px;"></div>
         <div>
-            <img src="img/cise-logo.jpg" style="display: block; width: 45%; height: auto; margin-left: auto; margin-right: auto;">
+            <img src="img/cise-logo.jpg" style="display: block; width: auto; height: auto; margin-left: auto; margin-right: auto;">
         </div>
         </form>
     </ion-content>
 </template>
 
 <script>
-    export default {
-        name: 'Login',
-        data() {
-            return {
-                input: {
-                    username: "",
-                    password: ""
-                }
-            }
-        },
-        methods: {
-            login() {
-                if(this.input.username != "" && this.input.password != "") {
-                    if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
-                        this.$emit("authenticated", true);
-                        this.$router.replace({ name: "home" });
-                    } else {
-                        alert("The username and / or password is incorrect");
-                    }
-                } else {
-                    alert("A username and password must be present");
-                }
-            }
+export default {
+  name: "Login",
+  data() {
+    return {
+      input: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    login() {
+      if (this.input.username != "" && this.input.password != "") {
+        if (
+          this.input.username == this.$parent.mockAccount.username &&
+          this.input.password == this.$parent.mockAccount.password
+        ) {
+          this.$emit("authenticated", true);
+          this.$router.replace({ name: "home" });
+        } else {
+          alert("The username and / or password is incorrect");
         }
+      } else {
+        alert("A username and password must be present");
+      }
     }
+  }
+};
 </script>
 
 <style scoped>
-    #login {
-        width: 500px;
-        border: 1px solid #CCCCCC;
-        background-color: #FFFFFF;
-        margin: auto;
-        margin-top: 200px;
-        padding: 20px;
-    }
+#login {
+  width: 500px;
+  border: 1px solid #cccccc;
+  background-color: #ffffff;
+  margin: auto;
+  margin-top: 200px;
+  padding: 20px;
+}
 </style>
-
-
