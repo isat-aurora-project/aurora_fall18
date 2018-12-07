a902c9080aad54bba02d208478c0658519bb55cf
Joy Williams <joywilliams@Joys-MacBook-Pro.local>
2018-12-07 11:32:45 -0500

    upload page working

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -2,8 +2,34 @@
   <div class="upload">
     <h1>Upload your Light-Show here!</h1>
     <p> Please upload your light-show as an Arduino file. </p>
-  <button class="btn upload">Upload</button>
+    <input type="file" @change="onFileSelected">
+    <button v-on:click.prevent="onUpload"> Upload</button>
   
   
   </div>
 </template>
+
+<script>
+import axios from "axios";
+export default {
+  data (){
+    return {
+      selectedFile: null
+    }
+  },
+  methods: {
+    onFileSelected(event){
+      this.selectedFile = event.target.files[0]
+    },
+    onUpload() {
+      const fd = new FormData();
+      fd.append('file', this.selectedFile, this.selectedFile.name)
+      axios.post('https://wt-a0f01f0b50faf36ea1feab5c1f6c544c-0.sandbox.auth0-extend.com/Aurora-mongoDB/submit',fd)
+      .then(res => {
+        console.log('test')
+      })
+    }
+  }
+}
+
+</script>
